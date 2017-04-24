a4124a685d58ad71c1cec0b6a17a733c9551d400
francafa <francafa@SES261552.saude.sc.gov.br>
2017-04-24 11:21:47 -0300

    Atualizações de help e index.

--- src/main/java/service/UsuarioRestService.java
+++ src/main/java/service/UsuarioRestService.java
@@ -15,7 +15,6 @@ import javax.ws.rs.core.Response;
 import dao.FactoryDao;
 import model.Usuario;
 
-
 @Path("/usuario")
 public class UsuarioRestService {
 
@@ -34,7 +33,7 @@ public class UsuarioRestService {
 	@Produces("application/json")
 	public Response create(Usuario usuario) {
 		FactoryDao.getUsuariodao().create(usuario);
-		return Response.ok().entity(new ServiceResponse(201,"Usuario Cadastrado")).build();
+		return Response.ok().entity(new ServiceResponse(201, "Usuario Cadastrado")).build();
 	}
 
 	@DELETE
@@ -42,16 +41,35 @@ public class UsuarioRestService {
 	@Produces("application/json")
 	public Response delete(@PathParam("cpf") String cpf) {
 		FactoryDao.getUsuariodao().delete(cpf);
-		return Response.ok().entity(new ServiceResponse(200,"Usuario Excluído")).build();
+		return Response.ok().entity(new ServiceResponse(200, "Usuario Excluído")).build();
 	}
 
 	@PUT
 	@Path("/update/{cpf}")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response update(@PathParam("cpf") String cpf,Usuario usuario) {
+	public Response update(@PathParam("cpf") String cpf, Usuario usuario) {
 		FactoryDao.getUsuariodao().update(usuario);
-		return Response.ok().entity(new ServiceResponse(200,"Usuario Atualizado")).build();
+		return Response.ok().entity(new ServiceResponse(200, "Usuario Atualizado")).build();
+	}
+
+	@GET
+	@Path("/help")
+	public Response help() {
+		String ajuda = "<H3>Serviços disponíveis para usuario:</H3> "
+				+ "<BR> A criação de usuário é realizada através endereço de requisição:"
+				+ "<BR>http://localhost:8080/UsuarioWeb/service/usuario/create"
+				+"<BR>Este serviço consome um JSON com os dados do usuário e retorna uma resposta também em JSON"
+				+ "<BR><BR> A atualização de usuário é realizada através do endereço de requisição:"
+				+ "<BR>http://localhost:8080/UsuarioWeb/service/usuario/update/{cpf do usuário}"
+				+"<BR>Este serviço consome um JSON com os dados do usuário e retorna uma resposta também em JSON"
+				+ "<BR><BR> A exclusão de usuário é realizada através do endereço de requisição:"
+				+ "<BR>http://localhost:8080/UsuarioWeb/service/usuario/delete/{cpf do usuário}"
+				+"<BR>Este serviço apenas retorna um JSON."
+				+ "<BR><BR> A listagem de todos os usuários é realizada através do endereço de requisição:"
+				+ "<BR>http://localhost:8080/UsuarioWeb/service/usuario/retrieve"
+				+"<BR>Este serviço retorna um JSON com todos os usuários cadastrados.";
+		return Response.ok().entity(ajuda).build();
 	}
 
 }
